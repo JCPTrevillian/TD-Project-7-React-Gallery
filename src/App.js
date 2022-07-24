c077b1636005dfc3d72bb3e74ce505151ee08e29
Jennifer Trevillian <95890754+JCPTrevillian@users.noreply.github.com>
2022-07-23 17:04:19 -0700

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Header from "./Header";
+import GalleryContainer from "./GalleryContainer.js";
+import PageNotFound from "./PageNotFound";
+import apiKey from "../config";
 
-export default App;
+class App extends Component {
+};
